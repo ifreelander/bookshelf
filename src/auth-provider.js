6bf13be41167854dcfb79d85f89dc26c1b63550b
ifreelander <roya.taleghani@gmail.com>
2020-10-29 16:39:19 +1100

    new branch

--- src/auth-provider.js
+++ src/auth-provider.js
@@ -4,7 +4,7 @@
 const localStorageKey = '__auth_provider_token__'
 
 async function getToken() {
-  // if we were a real auth provider, this is where we would make a request
+  // if we were a real auth provider, this is where we would make a request.
   // to retrieve the user's token. (It's a bit more complicated than that...
   // but you're probably not an auth provider so you don't need to worry about it).
   return window.localStorage.getItem(localStorageKey)
